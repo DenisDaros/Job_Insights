344ceeb3d2901aea29e7a08352744c56700f3a67
denis daros <denisv.daros@gmail.com>
2023-03-27 21:20:13 -0300

    agora vai

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,4 +2,4 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences("data/jobs.csv","python") == 1639
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
